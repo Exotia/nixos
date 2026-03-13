ac8b0d556645c68bf6b99ada5c42f833435997c1
Exotia <161851082+Exotia@users.noreply.github.com>
2026-03-13 11:55:33 +0000

    Update home.nix configuration with new packages and vars

--- home.nix
+++ home.nix
@@ -1,19 +1,84 @@
 { config, pkgs, ... }:
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
 
+  configs = {
+    hypr = "hypr";
+    nvim = "nvim";
+    wofi = "wofi";
+    rofi = "rofi";
+    waybar = "waybar";
+    kitty = "kitty";
+  };
+in
 {
+  imports = [
+    ./modules/theme.nix
+  ];
+
   home.username = "ole";
   home.homeDirectory = "/home/ole";
   home.stateVersion = "25.05";
-  programs.git.enable = true;
+
+  home.sessionVariables = {
+    NIXOS_PATH = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  };
+
+  home.sessionPath = [
+    "$HOME/.config/scripts"
+  ];
+
+  home.file.".local/share/fonts/omarchy.ttf".source = ./config/nixos/omarchy.ttf;
+
   programs.bash = {
     enable = true;
     shellAliases = {
-      btw = "echo i use nixos, btw";
+      btw = "echo i use hyprland btw";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos";
+      vim = "nvim";
     };
-    profileExtra = ''
-      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        exec uwsm start -S hyprland-uwsm.desktop
-      fi
+    initExtra = ''
+      export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
+      export PATH="$HOME/.config/scripts:$PATH"
+      nitch
     '';
   };
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    wofi
+    rofi
+    pcmanfm
+    alacritty
+    kitty
+    walker
+    uwsm
+    (pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        (nix-search-tv.overrideAttrs (old: {
+          env.GOEXPERIMENT = "jsonv2";
+          nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.removeReferencesTo ];
+          postFixup = (old.postFixup or "") + ''
+            remove-references-to -t ${pkgs.go} $out/bin/nix-search-tv
+          '';
+        }))
+      ];
+      text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
+    })
+  ];
+
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
 }
